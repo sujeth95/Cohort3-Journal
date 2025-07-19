b9dae5c3cd87c9956c04357eafe12be3949fed29
sujeth95 <sujeetoraon337@gmail.com>
2025-07-19 15:03:18 +0530

    Learning about JWT

--- Codes/Week-6.1/Practice/index.js
+++ Codes/Week-6.1/Practice/index.js
@@ -1,18 +1,11 @@
 const express = require("express")
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "sujeet124"
 const app = express();
 
 app.use(express.json());
 const users = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
 
 app.post("/signup", (req, res) => {
     const username = req.body.username;
@@ -46,8 +39,11 @@ app.post("/signin", (req, res) => {
     } 
     
     if(foundUser){
-        const token = generateToken();
-        foundUser.token = token;
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+
+        // foundUser.token = token;
 
         res.json({
             message: token
@@ -62,10 +58,13 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req,res) => {
     const token = req.headers.token;
+    const decodedInformation = jwt.verify(token, JWT_SECRET)
+    const username = decodedInformation.username
+    
     let foundUser = null
 
     for(let i=0; i<users.length;i++){
-        if(users[i].token == token){
+        if(users[i].username == username){
             foundUser = users[i]
         }
     }
