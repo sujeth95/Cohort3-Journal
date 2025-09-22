42f0a7d75a998e5d7ece032bd7254ee43990819c
sujeth95 <sujeetoraon337@gmail.com>
2025-09-22 20:09:51 +0530

    Revising HTML

--- Codes/Week-2.2/new/Revision/Promises3.js
+++ Codes/Week-2.2/new/Revision/Promises3.js
@@ -1,20 +1,35 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-function readTheFile(sendTheFinalValueHere) {
+// function readTheFile(sendTheFinalValueHere) {
 
-    fs.readFile("a.txt","utf-8", function(err,data) {
-        sendTheFinalValueHere(data);
-    })
+//     fs.readFile("a.txt","utf-8", function(err,data) {
+//         sendTheFinalValueHere(data);
+//     })
+// }
+
+// function readFile(fileName){
+//     // read the file and return its value.
+//     return new Promise(readTheFile);
+// }
+
+// const p = readFile();
+
+// function callback(contents) {
+//     console.log(contents);
+// }
+// p.then(callback);
+
+//--------------------------------------------
+//the real operation that we want to promisify.
+function doAsyncOp(resolve){
+    setTimeout(resolve, 3000);
 }
 
-function readFile(fileName){
-    // read the file and return its value.
-    return new Promise(readTheFile);
+function setTimeoutPromisified(){
+    return new Promise(doAsyncOp);
 }
 
-const p = readFile();
-
-function callback(contents) {
-    console.log(contents);
+function callback(){
+    console.log("3 seconds have passed");
 }
 p.then(callback);
