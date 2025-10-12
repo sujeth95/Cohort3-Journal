e642c8f58732c86f840a6d612c311a89a9f2c1e2
sujeth95 <sujeetoraon337@gmail.com>
2025-10-12 19:34:46 +0530

    Update V1.8

--- Codes/Week-8.1/backend/db.js
+++ Codes/Week-8.1/backend/db.js
@@ -1,25 +1,25 @@
-const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
 console.log("Connected to Mongoo");
-mongoose.connect("mongodb+srv://sujeet337:8CvsLEwDlSphN2C1@cluster0.sjari5s.mongodb.net/");
+mongoose.connect("mongodb+srv://sujeet337:8CvsLEwDlSphN2C1@cluster0.sjari5s.mongodb.net/coursera-app");
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-const userSchema = Schema({
+const userSchema = new Schema({
     email: { type: String, unique: true },
     password: String,
     firstName: String,
     lastName: String
 });
 
-const adminSchema = Schema({
+const adminSchema =new Schema({
     email: { type: String, unique: true },
     password: String,
     firstName: String,
     lastName: String
 });
 
-const courseSchema = Schema({
+const courseSchema = new Schema({
     title: String,
     description: String,
     price: Number,
@@ -27,15 +27,15 @@ const courseSchema = Schema({
     creatorId: ObjectId
 });
 
-const purchaseSchema = Schema({
+const purchaseSchema =new Schema({
     userId: ObjectId,
     courseId: ObjectId
 });
 
-const userModel = mongoose.Model("user", userSchema);
-const adminModel = mongoose.Model("admin", adminSchema);
-const courseModel = mongoose.Model("course", courseSchema);
-const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
 
 module.exports = {
     userModel,
--- Codes/Week-8.1/backend/index.js
+++ Codes/Week-8.1/backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
@@ -9,6 +11,12 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
+async function main() {
+    // dotenv folder.
+await mongoose.connect("mongodb+srv://sujeet337:8CvsLEwDlSphN2C1@cluster0.sjari5s.mongodb.net/coursera-app");
 app.listen(3000, () => {
     console.log("Running in port 3000.");
 })
+}
+
+main();
