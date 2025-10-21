ce1bc70a3206e2e2a9a9aa95e4b4492a8fc69520
sujeth95 <sujeetoraon337@gmail.com>
2025-10-21 13:03:41 +0530

    Learning Basics of React Part-III

--- Codes/Week-9/App2.jsx
+++ Codes/Week-9/App2.jsx
@@ -11,12 +11,35 @@ export default function App() {
     ]);
 
     function addTodo() {
-        setTodos()
+        let newArray = [];
+        for (let i = 0; i < todos.length; i++) {
+            newArray.push(todos[i]);
+        }
+        newArray.push({
+            title: "Eat food",
+            description: "eat food properly",
+            done: true,
+        });
     }
 
     return (
         <div>
+            <input id="title" type="text" placeholder="Title"></input>
+            <input id="description" type="text" placeholder="Description"></input>
+            <br />
             <button onClick={addTodo}>Add Todo</button>
+            <br />
+        <Todo title={"Sujeet"} description={"Sujeet Oraon"} done={false} />
         </div>
     );
+}
+
+function Todo(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.description}</h2>
+            <h1>{props.done ? "Task is done" : "Task is not done"}</h1>
+        </div>
+    )
 }
